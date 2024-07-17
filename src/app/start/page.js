8d62f3b21725c150ef596d0364dfeb479ecd5e56
itc-s23014 <s23014@std.it-college.ac.jp>
2024-07-17 10:37:04 +0900

    start push

--- src/app/start/page.js
+++ src/app/start/page.js
@@ -1,7 +1,21 @@
 //スタート画面
 const Start = () => {
     return (
-        <h1>Start:スタート</h1>
+        <main className="min-h-screen flex justify-center items-center">
+            <div className="w-[498px] h-[275px] bg-white relative">
+                <div className="absolute left-[77px] top-[93px] text-zinc-950 text-[40px] font-normal font-['Inter']">
+                    Teston
+                </div>
+                <div className="absolute left-[272px] top-[148px]">
+                    {/* Wrap "start" text with Link component for navigation */}
+                    <Link href="../question"
+                          className="block px-4 py-2 bg-red-400 rounded-[20px] text-white text-xl font-normal font-['Inter']">
+                        start
+
+                    </Link>
+                </div>
+            </div>
+        </main>
     )
 }
 
