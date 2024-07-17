09b307e0d6ca154d203f9438a2f364a2d5384f62
s23017 <s23017@std.it-college.ac.jp>
2024-07-17 10:32:17 +0900

    qestion作成

--- src/app/question/page.js
+++ src/app/question/page.js
@@ -1,8 +1,39 @@
 //出題画面
 const Question = () => {
     return (
-        <h1>Question:出題</h1>
-    )
+        // <h1>Question:出題</h1>
+    <main className="flex justify-center items-center h-screen">
+        <div className="w-full max-w-lg bg-white p-4">
+            <div className="grid grid-cols-2 gap-4">
+                <div className="col-span-2 text-center text-black text-xl font-normal font-['Inter']">問題文</div>
+                <Link href="../explanation/page.js" passHref>
+                    <div
+                        className="bg-red-400 rounded-2xl h-8 flex items-center justify-center text-white text-xl font-normal font-['Inter']">
+                        sec1
+                    </div>
+                </Link>
+                <Link href="../explanation/page.js" passHref>
+                    <div
+                        className="bg-red-400 rounded-2xl h-8 flex items-center justify-center text-white text-xl font-normal font-['Inter']">
+                        sec2
+                    </div>
+                </Link>
+                <Link href="../explanation" passHref>
+                    <div
+                        className="bg-red-400 rounded-2xl h-8 flex items-center justify-center text-white text-xl font-normal font-['Inter']">
+                        sec3
+                    </div>
+                </Link>
+                <Link href="../explanation/page.js" passHref>
+                    <div
+                        className="bg-red-400 rounded-2xl h-8 flex items-center justify-center text-white text-xl font-normal font-['Inter']">
+                        sec4
+                    </div>
+                </Link>
+            </div>
+        </div>
+    </main>
+)
 }
 
 export default Question
